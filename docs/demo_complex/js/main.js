d1daad8ccae2afacee7430fe4e56f82212b1191a
ics-ikeda <ikeda@ics-web.jp>
2022-03-29 18:33:13 +0900

    Angular 1系、Bootstrap UIの撤廃 / Vue 3への置き換え

--- docs/demo_complex/js/main.js
+++ docs/demo_complex/js/main.js
@@ -1,81 +1,71 @@
-/// <reference path="angularjs/angular.d.ts" />
-var demo;
-(function (demo) {
-    var css3;
-    (function (css3) {
-        /** プルダウンの一覧 */
-        var PULLDOWN_LIST = [
-            { isDivider: false, label: "通常", data: "normal" },
-            { isDivider: true, label: "", data: "" },
-            { isDivider: false, label: "比較(暗)", data: "darken" },
-            { isDivider: false, label: "乗算", data: "multiply" },
-            { isDivider: false, label: "焼き込み", data: "color-burn" },
-            { isDivider: true, label: "", data: "" },
-            { isDivider: false, label: "比較(明)", data: "lighten" },
-            { isDivider: false, label: "スクリーン", data: "screen" },
-            { isDivider: false, label: "覆い焼き", data: "color-dodge" },
-            { isDivider: true, label: "", data: "" },
-            { isDivider: false, label: "オーバーレイ", data: "overlay" },
-            { isDivider: false, label: "ソフトライト", data: "soft-light" },
-            { isDivider: false, label: "ハードライト", data: "hard-light" },
-            { isDivider: true, label: "", data: "" },
-            { isDivider: false, label: "差の絶対値", data: "difference" },
-            { isDivider: false, label: "除外", data: "exclusion" },
-            { isDivider: true, label: "", data: "" },
-            { isDivider: false, label: "色相", data: "hue" },
-            { isDivider: false, label: "彩度", data: "saturation" },
-            { isDivider: false, label: "カラー", data: "color" },
-            { isDivider: false, label: "輝度", data: "luminosity" },
-        ];
-        // AngularJS のアプリケーションインスタンス
-        var app = angular.module('ui.bootstrap.demo', ['ui.bootstrap']);
-        // ----------------------------------
-        // 全体の制御
-        // ----------------------------------
-        app.controller("MyAppCtrl", function ($scope, $log) {
-            $scope.currentBlendMode = "normal";
-            $scope.change = function (item) {
-                $scope.currentBlendMode = item.data;
-            };
-        });
-        // ----------------------------------
-        // カルーセルの制御
-        // ----------------------------------
-        app.controller('CarouselDemoCtrl', function ($scope) {
-            $scope.myInterval = 0;
-            var slides = $scope.slides = [];
-            $scope.addSlide = function () {
-                var newWidth = slides.length;
-                slides.push({
-                    image: 'imgs/bg_' + newWidth + '.jpg',
-                });
-            };
-            for (var i = 0; i < 5; i++) {
-                $scope.addSlide();
-            }
-        });
-        // ----------------------------------
-        // ドロップダウンメニューの制御
-        // ----------------------------------
-        app.controller('DropdownCtrl', function ($scope, $log) {
-            $scope.items = PULLDOWN_LIST;
-            // 初期値
-            $scope.currentLabel = "" + $scope.items[0].label + " (" + $scope.items[0].data + ")";
-            $scope.status = {
-                isopen: true
-            };
-            // イベントハンドラー
-            $scope.toggleDropdown = function ($event) {
-                $event.preventDefault();
-                $event.stopPropagation();
-                $scope.status.isopen = !$scope.status.isopen;
-            };
-            $scope.setAction = function (item) {
-                $scope.change(item);
-                $scope.currentLabel = "" + item.label + " (" + item.data + ")";
-                $scope.status.currentLabel = item.label;
-            };
-        });
-    })(css3 = demo.css3 || (demo.css3 = {}));
-})(demo || (demo = {}));
-//# sourceMappingURL=main.js.map
+import {
+  ref,
+  createApp,
+} from "https://unpkg.com/vue@3.2.21/dist/vue.esm-browser.js";
+
+/** プルダウンの一覧 */
+const blendModes = [
+  [{ label: "通常", data: "normal" }],
+  [
+    { label: "比較(暗)", data: "darken" },
+    { label: "乗算", data: "multiply" },
+    { label: "焼き込み", data: "color-burn" },
+  ],
+  [
+    { label: "比較(明)", data: "lighten" },
+    { label: "スクリーン", data: "screen" },
+    { label: "覆い焼き", data: "color-dodge" },
+  ],
+  [
+    { label: "オーバーレイ", data: "overlay" },
+    { label: "ソフトライト", data: "soft-light" },
+    { label: "ハードライト", data: "hard-light" },
+  ],
+  [
+    { label: "差の絶対値", data: "difference" },
+    { label: "除外", data: "exclusion" },
+  ],
+  [
+    { label: "色相", data: "hue" },
+    { label: "彩度", data: "saturation" },
+    { label: "カラー", data: "color" },
+    { label: "輝度", data: "luminosity" },
+  ],
+  [
+    { label: "plus-lighter", data: "plus-lighter" },
+    { label: "plus-darker", data: "plus-darker" },
+  ],
+];
+
+const slides = [...Array(5).keys()].map((value) => `imgs/bg_${value}.jpg`);
+
+const app = createApp({
+  setup() {
+    const currentBlendMode = ref("normal");
+    const isLook3d = ref(false);
+
+    const onChange = (event) => {
+      currentBlendMode.value = event.target.value;
+    };
+
+    const photoIndex = ref(0);
+
+    const onClickSlide = (index) => {
+      document
+        .querySelector(`.carouselCard:nth-child(${index + 1})`)
+        .scrollIntoView({behavior: "smooth"});
+      photoIndex.value = index;
+    };
+
+    return {
+      currentBlendMode,
+      isLook3d,
+      blendModes,
+      slides,
+      onChange,
+      photoIndex,
+      onClickSlide,
+    };
+  },
+});
+app.mount("#app");
